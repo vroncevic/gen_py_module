633628c3cc15e1d413ca03c6f2fad9087d298f27
Vladimir Roncevic <elektron.ronca@gmail.com>
2025-11-27 18:33:01 +0100

    [gen_py_module] Updated code coverage, version, docs

--- tests/run_coverage.sh
+++ tests/run_coverage.sh
@@ -9,10 +9,11 @@
 
 rm -rf htmlcov gen_py_module_coverage.xml gen_py_module_coverage.json .coverage
 rm -rf fresh_new/ full_simple_new/ latest_pro/ simple_read/ simple_write/
-ats_coverage_run.py -n gen_py_module -p ../README.md
-rm -rf fresh_new/ full_simple_new/ latest_pro/ simple_read/ simple_write/
 python3 -m coverage run -m --source=../gen_py_module unittest discover -s ./ -p '*_test.py' -vvv
 python3 -m coverage html -d htmlcov
 python3 -m coverage xml -o gen_py_module_coverage.xml 
 python3 -m coverage json -o gen_py_module_coverage.json
 python3 -m coverage report --format=markdown -m
+python3 ats_coverage.py -n gen_py_module
+rm htmlcov/.gitignore
+echo "Done"
